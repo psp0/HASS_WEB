69045534711cc5ab846ca79a1181f23d49fe21d7
psp0 <tkdvlf9058@g.hongik.ac.kr>
2024-11-28 00:43:07 +0900

    fix: 회수 요청에 대한 로직 추가

--- pages/worker/request/fetch_accept.php
+++ pages/worker/request/fetch_accept.php
@@ -6,7 +6,7 @@ $workerId = $_POST['special_worker'] ?? null;
 $visitDate = $_POST['visit_date'] ?? null;
 $requestId = $_POST['request_id'] ?? null;  
 
-if (isset($workerId, $visitDate, $requestId)) {
+if (isset($workerId, $requestId)) {
     try {
         // if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
         //   throw new Exception('로그인이 필요합니다.');
@@ -26,11 +26,12 @@ if (isset($workerId, $visitDate, $requestId)) {
         if (!oci_execute($beginStmt, OCI_NO_AUTO_COMMIT)) {
             throw new Exception(message: '트랜잭션 시작 실패');
         }
+        if(isset($visitDate )){
 
-        $formattedVisitDate = DateTime::createFromFormat('y.m.d H:i', $visitDate)->format('Y-m-d H:i:s');
-
-        // 쿼리 수정 (TO_DATE 함수 사용)
-        $visitQuery = "INSERT INTO VISIT (VISIT_ID, VISIT_DATE, DATE_CREATED, WORKER_ID, REQUEST_ID)
+          $formattedVisitDate = DateTime::createFromFormat('y.m.d H:i', $visitDate)->format('Y-m-d H:i:s');
+          
+          // 쿼리 수정 (TO_DATE 함수 사용)
+          $visitQuery = "INSERT INTO VISIT (VISIT_ID, VISIT_DATE, DATE_CREATED, WORKER_ID, REQUEST_ID)
             VALUES (V_SEQ.NEXTVAL, TO_DATE(:VISIT_DATE, 'YYYY-MM-DD HH24:MI:SS'), SYSDATE, :WORKER_ID, :REQUEST_ID)";
         
         $stmtVisit = oci_parse($conn, $visitQuery);
@@ -39,11 +40,24 @@ if (isset($workerId, $visitDate, $requestId)) {
         oci_bind_by_name($stmtVisit, ":REQUEST_ID", $requestId);
         
         if (!oci_execute($stmtVisit, OCI_NO_AUTO_COMMIT)) {
-            $e = oci_error($stmtVisit);
-            throw new Exception('visit 테이블 삽입 쿼리 실행 오류: ' . $e['message']);
-        }
-
-    oci_free_statement($stmtVisit);
+          $e = oci_error($stmtVisit);
+          throw new Exception('visit 테이블 삽입 쿼리 실행 오류: ' . $e['message']);
+        }        
+        oci_free_statement($stmtVisit);
+      }else{
+        $visitQuery = "INSERT INTO VISIT (VISIT_ID, VISIT_DATE, DATE_CREATED, WORKER_ID, REQUEST_ID)
+          VALUES (V_SEQ.NEXTVAL, (SELECT PREFER_DATE FROM REQUEST_PREFERENCE_DATE WHERE REQUEST_ID=:REQUEST_ID AND ROWNUM = 1), SYSDATE, :WORKER_ID, :REQUEST_ID)";
+      
+      $stmtVisit = oci_parse($conn, $visitQuery);
+      oci_bind_by_name($stmtVisit, ":WORKER_ID", $workerId);
+        oci_bind_by_name($stmtVisit, ":REQUEST_ID", $requestId);
+        
+        if (!oci_execute($stmtVisit, OCI_NO_AUTO_COMMIT)) {
+          $e = oci_error($stmtVisit);
+          throw new Exception('visit 테이블 삽입 쿼리 실행 오류: ' . $e['message']);
+        }        
+        oci_free_statement($stmtVisit);
+      }
   
     $updateRequestQuery = "UPDATE REQUEST SET  REQUEST_STATUS = '방문예정' WHERE REQUEST_ID = :REQUEST_ID";
     $updateRequestStmt = oci_parse($conn, $updateRequestQuery);
@@ -76,4 +90,3 @@ if (isset($workerId, $visitDate, $requestId)) {
     echo "<p class='error'>선택항목을 선택해주십시오.</p>";
   }
 ?>
-
