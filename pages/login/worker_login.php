f77f5eded2a2bc92fd99af5e9609f44995d193af
DongHyeon <ehdgus7513@g.hongik.ac.kr>
2024-11-28 18:48:55 +0900

    fix: 페이지 경로 수정

--- pages/login/worker_login.php
+++ pages/login/worker_login.php
@@ -138,7 +138,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $_SESSION['logged_in'] = true;
 
         echo "<script>alert('로그인 되었습니다. 환영합니다!');</script>";
-        echo "<script>location.href='pages/worker/main.php';</script>";
+        echo "<script>location.href='" . TEAM_PATH . "/pages/worker/main.php';</script>";
         exit;
         } else {
             echo "<script>alert('ID 또는 비밀번호가 잘못되었습니다.');</script>";
